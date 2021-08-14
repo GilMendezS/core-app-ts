68170f949f0899150ec5e8a2431ba696db7e388e
Gilberto Méndez <gilberto.mendez.santiz@gmail.com>
2021-08-14 11:43:43 -0500

    User models and migrations
    create migration and models files to generate the user entity using sequelize

---
+++ src/models/user.ts
@@ -0,0 +1,30 @@
+'use strict';
+import { Model, DataTypes, Optional } from 'sequelize';
+import database from '../config/database.connection'
+import UserAttributesI from './interfaces/user.interface';
+
+export class UserModel extends Model implements UserAttributesI{
+  id?: Number | undefined;
+  username!: string;
+  password!: string;
+  active_session!: boolean;
+  token!: string;
+  status!: string;
+  customer_id!: Number;
+}
+UserModel.init( {
+  username: DataTypes.STRING,
+  password: DataTypes.STRING,
+  active_session: DataTypes.BOOLEAN,
+  token: DataTypes.STRING,
+  status: {
+    type: DataTypes.ENUM('active', 'inactive'),
+    defaultValue: 'active',
+  },
+  customer_id: DataTypes.INTEGER
+}, {
+  sequelize: database,
+  modelName: 'User',
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});

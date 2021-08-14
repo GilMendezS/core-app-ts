54f3169b87511093da83ca392e289051eeb25069
Gilberto Méndez <gilberto.mendez.santiz@gmail.com>
2021-08-13 20:54:22 -0500

    configure database
    database connetion setup and strucure for models, seeders and migrations using sequelize-cli

--- src/config/application.ts
+++ src/config/application.ts
@@ -1,3 +1,5 @@
+import { Dialect } from 'sequelize';
+
 import dotenv from 'dotenv';
 
 class ApplicationEnvs {
@@ -10,6 +12,12 @@ class ApplicationEnvs {
     loadEnvs() {
         return {
             port: process.env.PORT || 3000,
+            database: process.env.DATABASE_NAME ?? '',
+            db_username: process.env.DATABASE_USERNAME ?? '',
+            db_password: process.env.DATABASE_PASSWORD ?? '',
+            db_host: process.env.DATABASE_HOST ?? '',
+            db_port: process.env.DATABASE_PORT as unknown as number,
+            db_dialect: process.env.DATABASE_DIALECT as Dialect
         }
     }
 
---
+++ src/config/database.connection.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+import AppConfig from './application';
+
+const db: Sequelize = new Sequelize(
+    AppConfig.loadEnvs().database,
+    AppConfig.loadEnvs().db_username,
+    AppConfig.loadEnvs().db_password,
+    {
+        host: AppConfig.loadEnvs().db_host,
+        port: AppConfig.loadEnvs().db_port,
+        dialect: AppConfig.loadEnvs().db_dialect
+    },
+);
+
+export default db;
